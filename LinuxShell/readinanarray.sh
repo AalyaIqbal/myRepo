d04cf1c4f5ec986866f49b1e0ee1fedca7531abf
AalyaIqbal <63206070+AalyaIqbal@users.noreply.github.com>
2020-04-06 14:33:24 +0530

    Create readinanarray.sh

---
+++ LinuxShell/readinanarray.sh
@@ -0,0 +1,13 @@
+#Given a list of countries, each on a new line, your task is to read them into an array and then display the entire array, with a space between each of the countries' names.
+
+while read line
+
+do
+
+    arr=(${arr[@]} $line)
+
+done
+
+
+
+echo ${arr[@]}
